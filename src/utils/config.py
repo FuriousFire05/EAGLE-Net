a1fe1aebaaead85167ba9b0f69fd9ba815cea97e
Furious-Fire <136786792+Furious-Fire@users.noreply.github.com>
2026-04-28 12:11:44 +0400

    Upgrade config to centralized experiment control

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,18 +1,51 @@
 # src/utils/config.py
 
 CONFIG = {
+
+    # ========================
+    # MODEL SETTINGS
+    # ========================
     "model": {
-        "name": "eagle_net",# change this only
+        "name": "baseline_cnn",   # change to: baseline_cnn / lightweight_cnn / eagle_net
         "num_classes": 10
     },
 
-    "training": {
+    # ========================
+    # DATA SETTINGS
+    # ========================
+    "data": {
+        "root": "data/raw",
+        "image_size": 64,
+
         "batch_size": 32,
-        "epochs": 3,
-        "learning_rate": 0.001
+        "num_workers": 2,
+
+        "train_split": 0.70,
+        "val_split": 0.15,
+        "test_split": 0.15,
+
+        "seed": 42
     },
 
+    # ========================
+    # TRAINING SETTINGS
+    # ========================
+    "training": {
+        "epochs": 10,
+        "learning_rate": 0.001,
+        "weight_decay": 1e-4,
+
+        # scheduler
+        "scheduler_step_size": 5,
+        "scheduler_gamma": 0.5
+    },
+
+    # ========================
+    # PATHS
+    # ========================
     "paths": {
-        "model_dir": "artifacts/models/"
+        "model_dir": "artifacts/models",
+        "results_dir": "artifacts/results",
+        "plots_dir": "artifacts/plots"
     }
 }
